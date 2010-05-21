7d9ef0e84b29fc5ca339b3fc2e4bec43b3d6b206
João Portela <ei04034+github@gmail.com>
2010-05-21 13:23:17 +0100

    added rule to make my live easier.

--- plugable_coach/makefile.d
+++ plugable_coach/makefile.d
@@ -1,4 +1,4 @@
 assistantcoach.o: assistantcoach.cpp assistantcoach.hpp
 dieroll.o: dieroll.cpp
 main.o: main.cpp assistantcoach.hpp dieroll.hpp
-test1.o: test1.cpp syncronizedqueue.hpp
+syncqueue_example.o: syncqueue_example.cpp syncronizedqueue.hpp

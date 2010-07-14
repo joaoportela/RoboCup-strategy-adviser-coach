64bfa76111455e1b441dd25bf81132fa51da8773
João Portela <ei04034+github@gmail.com>
2010-07-14 18:47:14 +0100

    exit on unused variables

--- statistics_calculator/SoccerScope2/run.sh
+++ statistics_calculator/SoccerScope2/run.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
+set -u
+
 MYCLASSPATH=soccerscope.jar:java-xmlbuilder-0.3.jar
-echo $#
 if [ $# -eq 2 ];then
 	batch="--batch"
 	RCG=$1
@@ -13,3 +14,4 @@ else
 fi
 
 CLASSPATH=${MYCLASSPATH} java soccerscope.SoccerScope "${batch}" "${RCG}" "${XML}" > soccerscope.out 2> soccerscope.err
+echo "return value $?"
